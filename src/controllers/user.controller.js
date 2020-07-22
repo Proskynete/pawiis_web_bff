ab113699301d1c2a7cf64d3573800227fe3f34ee
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-22 17:52:19 -0400

    Handler success and error response - post user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,21 @@
+const axios = require('axios');
+const config = require('../config/config.js');
+
 const signIn = async (req, res) => {
 	const { email, password } = req.body;
+	try {
+		const { data } = await axios.post(
+			`${config.login_service}/signin?email=${email}&password=${password}`
+		);
+
+		return res.status(200).json({
+			user: data.user,
+		});
+	} catch (error) {
+		return res.status(error.response.status).json({
+			message: error.response.data.message,
+		});
+	}
 };
 
 const getUserById = (req, res) => {};
