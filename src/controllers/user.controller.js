21f90778e3c3ea6b952b9ab85f4c176cecbdc3c3
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-22 16:46:21 -0400

    Create user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,14 @@
+const signIn = async (req, res) => {};
+
+const getUserById = (req, res) => {};
+
+const updateUser = (req, res) => {};
+
+const deleteUser = (req, res) => {};
+
+module.exports = {
+	signIn,
+	getUserById,
+	updateUser,
+	deleteUser,
+};
