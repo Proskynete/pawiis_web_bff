d540dc53a321a05b0bb1739952f1e5c8b0a37667
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 13:24:03 -0400

    Create get action to logs services

--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -25,13 +25,10 @@ const createNewPet = async (req, res) => {
 const getPet = async (req, res) => {
 	const { owner } = req.query;
 	try {
-		const { data } = await axios.get(
-			`${config.pet_service}/get?owner=${owner}`
+		const pet = await axios.get(`${config.pet_service}/get?owner=${owner}`);
+		const log = await axios.get(
+			`${config.log_service}/get?pet_id=${pet.data.pet._id}`
 		);
-
-		return res.status(200).json({
-			pet: data.pet,
-		});
 	} catch (error) {
 		return res.status(error.response.status).json({
 			message: error.response.data.message,
